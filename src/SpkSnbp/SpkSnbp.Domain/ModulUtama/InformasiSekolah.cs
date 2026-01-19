c2e1395f830dab00600a0e57bbe7a14e8f842c06
AdiJT <aditaklal@gmail.com>
2026-01-19 17:04:14 +0800

    S

---
+++ src/SpkSnbp/SpkSnbp.Domain/ModulUtama/InformasiSekolah.cs
@@ -0,0 +1,39 @@
+﻿using SpkSnbp.Domain.Abstracts;
+
+namespace SpkSnbp.Domain.ModulUtama;
+
+public class InformasiSekolah : Entity<int>
+{
+    public static readonly InformasiSekolah Default = new()
+    {
+        NPSN = "50304998",
+        NamaSekolah = "SMKN1 Kota Kupang",
+        BentukPendidikan = "SMK",
+        Akreditasi = "A",
+        Nilai = 92,
+        NoSKAkreditasi = "1857/BAN-SM/SK/2022",
+        TanggalSKAkreditasi = new(2022, 11, 30),
+        TMTMulaiSKAkreditasi = new(2022, 11, 30),
+        TMTSelesaiSKAkreditasi = new(2027, 12, 31),
+        KepalaSekolah = "Mixon Rudolf Nicolas Abineno",
+        NoHP = "081313474144",
+    };
+
+    public required string NPSN { get; set; }
+    public required string NamaSekolah { get; set; }
+    public required string BentukPendidikan { get; set; }
+    public required string Akreditasi { get; set; }
+    public required double Nilai { get; set; }
+    public required string NoSKAkreditasi { get; set; } 
+    public required DateOnly TanggalSKAkreditasi { get; set; } 
+    public required DateOnly TMTMulaiSKAkreditasi { get; set; } 
+    public required DateOnly TMTSelesaiSKAkreditasi { get; set; } 
+    public required string KepalaSekolah { get; set; } 
+    public required string NoHP { get; set; } 
+}
+
+public interface IInformasiSekolahRepository
+{
+    Task<InformasiSekolah> Get();
+    void Update(InformasiSekolah informasiSekolah);
+}
---
+++ src/SpkSnbp/SpkSnbp.Infrastructure/ModulUtama/InformasiSekolah.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SpkSnbp.Domain.ModulUtama;
+using SpkSnbp.Infrastructure.Database;
+
+namespace SpkSnbp.Infrastructure.ModulUtama;
+
+internal class InformasiSekolahConfiguration : IEntityTypeConfiguration<InformasiSekolah>
+{
+    public void Configure(EntityTypeBuilder<InformasiSekolah> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
+
+//internal class InformasiSekolahRepository : IInformasiSekolahRepository
+//{
+//    private readonly AppDbContext _appDbContext;
+
+//    public InformasiSekolahRepository(AppDbContext appDbContext)
+//    {
+//        _appDbContext = appDbContext;
+//    }
+
+//    public async Task<InformasiSekolah> Get()
+//    {
+//        var informasiSekolah = await _appDbContext.InformasiSekolah.FirstOrDefaultAsync();
+//        if (informasiSekolah is null)
+//        {
+//            informasiSekolah = InformasiSekolah.Default;
+//            _appDbContext.InformasiSekolah.Add(informasiSekolah);
+//            await _appDbContext.SaveChangesAsync();
+//        }
+//    }
+
+//    public void Update(InformasiSekolah informasiSekolah)
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
